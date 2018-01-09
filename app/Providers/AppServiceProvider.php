6a43bc68ed151b4630161d2281d0e4c7b13f2025
Junior Frogie <Frogie@Junior-Frogie.local>
2018-01-10 03:17:55 +0700

    Fix error share major

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,30 +3,33 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 use App\Major;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-        // all views can access to major
-        $majors = Major::orderBy('name')->get();
-        view()->share('majors', $majors);
+  /**
+  * Bootstrap any application services.
+  *
+  * @return void
+  */
+  public function boot()
+  {
+    //
+    // all views can access to major
+    if (Schema::hasTable('majors')) {
+      $majors = Major::orderBy('name')->get();
+      view()->share('majors', $majors);
     }
+  }
 
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+  /**
+  * Register any application services.
+  *
+  * @return void
+  */
+  public function register()
+  {
+    //
+  }
 }
